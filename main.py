fe39ad67cd2b124b1fdde46cfd5ec7641bdc5625
Nikolas Schildberg <nikolas.schildberg@gmail.com>
2022-03-15 00:40:34 -0300

    Glue all together by main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+"""Runs other modules in folder, plotting data from year budget.
+Build tables (and export to csv) like the models provided, in order
+to explore your household budget, too."""
+
+import categories
+import yearly_summary
+import month_balance_tracker
